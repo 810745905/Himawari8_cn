a6ae578a26b15ec95b9c726d72cdb5a65221c8ef
LiaoYingting <32995184+810745905@users.noreply.github.com>
2019-04-26 17:10:16 +0800

    #画图方式改变

--- __main__.py
+++ __main__.py
@@ -4,34 +4,32 @@ from lib import drawpic_CN
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PIC_FILE_0 = os.path.join(BASE_DIR, '0')
-PIC_FILE_1 = os.path.join(BASE_DIR, '1')
-PIC_FILE_2 = os.path.join(BASE_DIR, '2')
-PIC_FILE_3 = os.path.join(BASE_DIR, '3')
-PIC_FILE_4 = os.path.join(BASE_DIR, '4')
-ROUTE_CONFIG_FILE = os.path.join(BASE_DIR, 'latlon', 'center.json')
+ROUTE_CONFIG_FILE = os.path.join(BASE_DIR, 'latlon', 'center4_4.json')
 data_path = os.path.join(BASE_DIR, 'data', os.listdir(os.path.join(BASE_DIR, 'data'))[0])
+lon_path = os.path.join(BASE_DIR, 'latlon', 'lon.csv')
+lat_path = os.path.join(BASE_DIR, 'latlon', 'lat.csv')
 
 with open(ROUTE_CONFIG_FILE, 'r') as f:
     f = json.load(f)
-channel = 'IR5'
-a = drawpic_CN.DrawConvectiveCloud(data_path, channel)
+channel = 'VIS'
+a = drawpic_CN.DrawConvectiveCloud(data_path, lon_path, lat_path, channel)
 a.readlatlon()
 
 for center in f:
     picname0 = os.path.join(PIC_FILE_0, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
-    a.draw(center, picname0, PAD_DOT=-56, DPI=100)
+    a.draw(center, picname0, pad_dot=-11.2, dpi=100)
 
-    picname1 = os.path.join(PIC_FILE_1, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
-    a.draw(center, picname1, PAD_DOT=-104.9, DPI=200)
+    # picname1 = os.path.join(PIC_FILE_1, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
+    # a.draw(center, picname1, PAD_DOT=-104.9, DPI=200)
+    #
+    # picname2 = os.path.join(PIC_FILE_2, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
+    # a.draw(center, picname2, PAD_DOT=-155, DPI=300)
 
-    picname2 = os.path.join(PIC_FILE_2, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
-    a.draw(center, picname2, PAD_DOT=-155, DPI=300)
-
-    picname3 = os.path.join(PIC_FILE_3, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
-    a.draw(center, picname3, PAD_DOT=-204.5, DPI=400)
-
-    picname4 = os.path.join(PIC_FILE_4, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
-    a.draw(center, picname4, PAD_DOT=-254.5, DPI=500)
+    # picname3 = os.path.join(PIC_FILE_3, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
+    # a.draw(center, picname3, PAD_DOT=-204.5, DPI=400)
+    #
+    # picname4 = os.path.join(PIC_FILE_4, f'{str(center[0]+1)}_{str(center[0]-1)}_{str(center[1]-1)}_{str(center[1]+1)}.png')
+    # a.draw(center, picname4, PAD_DOT=-254.5, DPI=500)
 
 
 
--- lib/drawpic_CN.py
+++ lib/drawpic_CN.py
@@ -1,13 +1,17 @@
-# import cmaps
+import cmaps
 import h5py
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 
 class DrawConvectiveCloud(object):
-    def __init__(self, data_path, channel):
+    def __init__(self, data_path, lon_path, lat_path,channel):
         self.data_path = data_path
+        self.lon_path = lon_path
+        self.lat_path = lat_path
         self.channel = channel
         self.sat_data = list()
         self.lat = list()
@@ -18,47 +22,41 @@ class DrawConvectiveCloud(object):
         data = h5file.get(self.channel)
         info = list(data.attrs.items())
         sat_data = data.get('Data')[:]
+        print(sat_data.max())
+        print(sat_data.min())
         self.sat_data = sat_data.T
-        Prjct = str(info[3][1], 'utf-8')
-        Width = info[4][1]
-        Height = info[5][1]
-        W0 = info[6][1]
-        E0 = info[7][1]
-        Space = info[8][1]
+        self.lon = np.loadtxt(open(self.lon_path, "rb"), delimiter=",", skiprows=0)
+        self.lat = np.loadtxt(open(self.lat_path,"rb"),delimiter=",",skiprows=0)
 
-        if Prjct == 'E':
-            la = (Height * Space / 100.0 - Space / 100.0) / 2
-            Minlat = W0 - la
-            Maxlat = W0 + la
-            lo = (Width * Space / 100.0 - Space / 100.0) / 2
-            Minlon = E0 - lo
-            Maxlon = E0 + lo
-
-        lon = list()
-        lat = list()
-        for x in range(0, Width):
-            for y in range(0, Height):
-                lon.append((Maxlon - Minlon) * x / Width + Minlon)
-                lat.append((Maxlat - Minlat) * (Height - y) / Height + Minlat)
-        self.lon = np.array(lon).reshape(1920, 1080)
-        self.lat = np.array(lat).reshape(1920, 1080)
-
-    def draw(self, center, picname, PAD_DOT=0, DPI=0):
-        fig = plt.figure(figsize=(40, 40), dpi=DPI)
+    def draw(self, center, picname, pad_dot=0, dpi=0):
+        fig = plt.figure(figsize=(10, 10), dpi=dpi)
         ax = fig.add_subplot(111)
         ax.set_axis_off()
         m = Basemap(
             epsg=3857,
-            llcrnrlon=center[1] - 1, llcrnrlat=center[0]-1,
-            urcrnrlon=center[1] + 1, urcrnrlat=center[0]+1,
+            llcrnrlon=center[1]-2, llcrnrlat=center[0]-2,
+            urcrnrlon=center[1]+2, urcrnrlat=center[0]+2,
             resolution='l', area_thresh=10000
         )
         x, y = m(self.lon, self.lat)  # 将lats / lons转换为地图投影坐标
-        m.contourf(x, y, self.sat_data)
+        # ct = np.array([[0, 151, 1], [255 , 255, 0],[250, 203, 1], [252, 123, 0],
+        #                [254, 0, 0],[200, 1, 0], [147, 2, 0],[255, 0, 254]]) / 255
+        # # m.contourf(x, y, self.sat_data, [750, 850], colors=[ct[0], ct[0]])
+        # m.contourf(x, y, self.sat_data, [850, 875], colors=[ct[0], ct[0]])
+        # m.contourf(x, y, self.sat_data, [875, 900], colors=[ct[1], ct[1]])
+        # m.contourf(x, y, self.sat_data, [900, 925], colors=[ct[2], ct[2]])
+        # m.contourf(x, y, self.sat_data, [925, 950], colors=[ct[3], ct[3]])
+        # m.contourf(x, y, self.sat_data, [950, 975], colors=[ct[4], ct[4]])
+        # m.contourf(x, y, self.sat_data, [975, 1000], colors=[ct[5], ct[5]])
+        # m.contourf(x, y, self.sat_data, [1000, 1020], colors=[ct[6], ct[6]])
+        # m.contourf(x, y, self.sat_data, [1020, 1500], colors=[ct[7], ct[7]])
+        levels = np.linspace(680, 1050, 50)
+        m.contourf(x, y, self.sat_data, levels=levels, cmap=cmaps.selfgray)
+        plt.show()
         plt.savefig(picname,
                     transparent=True,
                     bbox_inches='tight',
-                    pad_inches=PAD_DOT / DPI
+                    pad_inches=pad_dot / dpi
                     )
         plt.close()
 
